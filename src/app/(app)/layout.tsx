1c44ad2c2ac544fa12c020ce2c9e4373369f6569
Rebelein App <rebelein.app@gmail.com>
2025-10-17 10:12:34 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,6 +3,7 @@
 import { AppShell } from '@/components/layout/app-shell';
 import { AppProvider } from '@/context/AppContext';
 import { FirebaseClientProvider } from '@/firebase';
+import React from 'react';
 
 export default function AppLayout({
   children,
