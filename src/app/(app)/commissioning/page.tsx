a73b6acd39d6be92bf3c732041846e8100a5139c
Rebelein App <rebelein.app@gmail.com>
2025-10-23 08:43:48 +0000

    Hi

--- src/app/(app)/commissioning/page.tsx
+++ src/app/(app)/commissioning/page.tsx
@@ -3,7 +3,7 @@
 
 import * as React from 'react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
